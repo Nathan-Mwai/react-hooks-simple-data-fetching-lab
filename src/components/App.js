331e67f50bcb89554060efbef7b3b5497625f5be
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-08 08:18:54 +0300

    Adds dogs

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+
+    const [images, setImages] = useState('')
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() =>{
+        fetch(`https://dog.ceo/api/breeds/image/random`,)
+        .then(res => res.json())
+        .then(images => {
+            setImages(images.message)
+            setIsLoaded(true);
+        })
+    },[])
+
+    if (!isLoaded) return <p>Loading...</p>;
+  return (
+    <>
+    <img src={images} alt='A Random Dog'/>
+    </>
+  )
+}
+
+export default App
